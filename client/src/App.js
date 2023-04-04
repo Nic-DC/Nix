e6a38296d5015c4a70fef06bc1620fe38799e5b0
Nic <nic.dan.costea@gmail.com>
2023-04-04 09:48:37 +0200

    Skills

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,18 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./components/NavBar";
 import Banner from "./components/Banner";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <div className="App">
+      {/* <div id="page-content"> */}
       <NavBar />
       <Banner />
+      <Skills />
+      <Projects />
+      {/* </div> */}
     </div>
   );
 }
